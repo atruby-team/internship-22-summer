a717a8bc1d059113c13b00e9ff659999a3102a52
Hau Phan T. VN.Danang <hau.phan@monstar-lab.com>
2022-04-29 16:15:48 +0700

    W2-D1, Hậu Phan : Fix comment bài tập 3

--- mvn-hauphan/w1-d3/oop-module-include-extend/exercise.rb
+++ mvn-hauphan/w1-d3/oop-module-include-extend/exercise.rb
@@ -3,6 +3,7 @@
 Exercise: Create some classes or modules following:
 
 BbcNews:
+
 With attributes: id, title, content and category.
 With class methods:
 new(id, title, content, category): create new a BBC news
@@ -10,11 +11,109 @@ update(bbc_news, title, content): update title or content for bbc_news.
 delete(bbc_news): remove the bbc_news.
 
 CnnNews:
+
 With attributes: id, title, content and publication_date.
 With class methods:
 new(id, title, content, publication_date): create new a CNN news
 update(cnn_news, title, content): update title or content for cnn_news.
 delete(cnn_news): remove the cnn_news.
+
+Examples:
+
+bbc_news_01 = BbcNews.new(01, "BBC first news", "Hello world from BBC", "Say hello")
+bbc_news_02 = BbcNews.new(02, "BBC second news", "Hello Ruby from BBC", "Say hello")
+bbc_news_03 = BbcNews.new(02, "BBC third news", "Hello AsianTech from BBC", "Say hello")
+
+cnn_news_01 = CnnNews.new(01, "CNN first news", "Hello world from CNN", "06/07/2018")
+cnn_news_02 = CnnNews.new(02, "CNN second news", "Hello Ruby from CNN", "06/07/2018")
+cnn_news_03 = CnnNews.new(02, "CNN third news", "Hello AsianTech from CNN", "06/07/2018")
+
+BbcNews.update(bbc_news_02, "BBC second news!", "Hello Ruby from Asiantech")
+BbcNews.delete(bbc_news_01)
+
+CnnNews.update(cnn_news_02, "CNN second news!", "Hello Ruby from Asiantech")
+CnnNews.delete(cnn_news_03)
+
+=end
+
+require "pry"
+
+class News
+  attr_accessor :id
+  attr_accessor :title
+  attr_accessor :content
+  @@news = []
+
+  def self.add_new(new_obj)
+    @@news.push(new_obj)
+  end
+
+  def self.get_news
+    @@news
+  end
+
+  def self.update_new(new_obj, title, content)
+    return unless @@news.include?(new_obj)
+
+    index = @@news.index(new_obj)
+    @@news[index].title = title
+    @@news[index].content = content
+  end
+
+  def self.delete_new(new_obj)
+    return unless @@news.include?(new_obj)
+
+    @@news.delete(new_obj)
+  end
+end
+
+class BbcNews < News
+  attr_accessor :category
+  
+  def initialize(id, title, content, category)
+    @id = id
+    @title = title
+    @content = content
+    @category = category
+  end
+end
+
+bbc_news_01 = BbcNews.new(01, "BBC first news", "Hello world from BBC", "Say hello")
+bbc_news_02 = BbcNews.new(02, "BBC second news", "Hello Ruby from BBC", "Say hello")
+bbc_news_03 = BbcNews.new(03, "BBC third news", "Hello AsianTech from BBC", "Say hello")
+BbcNews.add_new(bbc_news_01)
+BbcNews.add_new(bbc_news_02)
+BbcNews.add_new(bbc_news_03)
+# p BbcNews.get_news
+BbcNews.update_new(bbc_news_02, "BBC second news!", "Hello Ruby from Asiantech")
+# p BbcNews.get_news
+BbcNews.delete_new(bbc_news_01)
+# p BbcNews.get_news
+
+class CnnNews < News
+  attr_accessor :publication_date
+
+  def initialize(id, title, content, publication_date)
+    @id = id
+    @title = title
+    @content = content
+    @publication_date = publication_date
+  end
+end
+
+cnn_news_01 = CnnNews.new(01, "CNN first news", "Hello world from CNN", "06/07/2018")
+cnn_news_02 = CnnNews.new(02, "CNN second news", "Hello Ruby from CNN", "06/07/2018")
+cnn_news_03 = CnnNews.new(03, "CNN third news", "Hello AsianTech from CNN", "06/07/2018")
+CnnNews.add_new(cnn_news_01)
+CnnNews.add_new(cnn_news_02)
+CnnNews.add_new(cnn_news_03)
+# p CnnNews.get_news
+CnnNews.update_new(cnn_news_02, "CNN second news!", "Hello Ruby from Asiantech")
+# p CnnNews.get_news
+BbcNews.delete_new(cnn_news_03)
+# p CnnNews.get_news
+
+=begin
 
 User:
 With attributes: id, name, friends, messages.
@@ -31,19 +130,6 @@ conversation(friend): Show conversation between user and that friend, note: Late
   dialog. Format of conversation dialog: "name" => "message" or "name(id)" => "message" if they are same name.
 
 Examples:
-bbc_news_01 = BbcNews.new(01, "BBC first news", "Hello world from BBC", "Say hello")
-bbc_news_02 = BbcNews.new(02, "BBC second news", "Hello Ruby from BBC", "Say hello")
-bbc_news_03 = BbcNews.new(02, "BBC third news", "Hello AsianTech from BBC", "Say hello")
-
-cnn_news_01 = CnnNews.new(01, "CNN first news", "Hello world from CNN", "06/07/2018")
-cnn_news_02 = CnnNews.new(02, "CNN second news", "Hello Ruby from CNN", "06/07/2018")
-cnn_news_03 = CnnNews.new(02, "CNN third news", "Hello AsianTech from CNN", "06/07/2018")
-
-BbcNews.update(bbc_news_02, "BBC second news!", "Hello Ruby from Asiantech")
-BbcNews.delete(bbc_news_01)
-
-CnnNews.update(cnn_news_02, "CNN second news!", "Hello Ruby from Asiantech")
-CnnNews.delete(cnn_news_03)
 
 first_user = User.new(01, "Bob")
 second_user = User.new(02, "Smith")
@@ -97,107 +183,6 @@ third_user.conversation(first_user) # []
 
 =end
 
-require "pry"
-
-class BbcNews 
-  attr_accessor :id
-  attr_accessor :title
-  attr_accessor :content
-  attr_accessor :categoty
-  @@bbc_news = []
-
-  def initialize(id, title, content, category)
-    @id = id
-    @title = title
-    @content = content
-    @category = category
-  end
-
-  def self.add_bbc_news(bbc_new)
-    @@bbc_news.push(bbc_new)
-  end
-
-  def self.get_news
-    @@bbc_news
-  end
-
-  def self.update_bbc_news(bbc_new, title, content)
-    return unless @@bbc_news.include?(bbc_new)
-
-    index = @@bbc_news.index(bbc_new)
-    @@bbc_news[index].title = title
-    @@bbc_news[index].content = content
-  end
-
-  def self.delete_bbc_news(bbc_new)
-    return unless @@bbc_news.include?(bbc_new)
-    @@bbc_news.delete(bbc_new)
-  end
-end
-
-bbc_news_01 = BbcNews.new(01, "BBC first news", "Hello world from BBC", "Say hello")
-bbc_news_02 = BbcNews.new(02, "BBC second news", "Hello Ruby from BBC", "Say hello")
-bbc_news_03 = BbcNews.new(03, "BBC third news", "Hello AsianTech from BBC", "Say hello")
-BbcNews.add_bbc_news(bbc_news_01)
-BbcNews.add_bbc_news(bbc_news_02)
-BbcNews.add_bbc_news(bbc_news_03)
-# p BbcNews.get_bbc_news
-BbcNews.update_bbc_news(bbc_news_02, "BBC second news!", "Hello Ruby from Asiantech")
-# p BbcNews.get_bbc_news
-BbcNews.delete_bbc_news(bbc_news_01)
-# p BbcNews.get_bbc_news
-
-class CnnNews 
-  attr_accessor :id
-  attr_accessor :title
-  attr_accessor :content
-  attr_accessor :publication_date
-  @@cnn_news = []
-
-  def initialize(id, title, content, publication_date)
-    @id = id
-    @title = title
-    @content = content
-    @publication_date = publication_date
-  end
-
-  def self.add_cnn_news(cnn_new)
-    @@cnn_news.push(cnn_new)
-  end
-
-  def self.get_news
-    @@cnn_news
-  end
-
-  def self.update_cnn_news(cnn_new, title, content)
-    return unless @@cnn_news.include?(cnn_new)
-
-    index = @@cnn_news.index(cnn_new)
-    @@cnn_news[index].title = title
-    @@cnn_news[index].content = content
-  end
-
-  def self.delete_cnn_news(cnn_new)
-    return unless @@cnn_news.include?(cnn_new)
-
-    @@cnn_news.delete(cnn_new)
-  end
-end
-
-
-
-cnn_news_01 = CnnNews.new(01, "CNN first news", "Hello world from CNN", "06/07/2018")
-cnn_news_02 = CnnNews.new(02, "CNN second news", "Hello Ruby from CNN", "06/07/2018")
-cnn_news_03 = CnnNews.new(03, "CNN third news", "Hello AsianTech from CNN", "06/07/2018")
-CnnNews.add_cnn_news(cnn_news_01)
-CnnNews.add_cnn_news(cnn_news_02)
-CnnNews.add_cnn_news(cnn_news_03)
-# p CnnNews.get_cnn_news
-CnnNews.update_cnn_news(cnn_news_02, "CNN second news!", "Hello Ruby from Asiantech")
-# p CnnNews.get_cnn_news
-BbcNews.delete_bbc_news(cnn_news_03)
-# p CnnNews.get_cnn_news
-
 class User 
   attr_accessor :id
   attr_accessor :name
@@ -235,7 +220,14 @@ class User
     return unless @organisations.include?(organisation)
 
     index = @organisations.find_index(organisation)
-    @organisations[index].get_news
+    news = @organisations[index].get_news.last(10)
+    news.map do |x| 
+      if x.class == BbcNews
+        { title: x.title, content: x.content, category: x.category } 
+      else
+        { title: x.title, content: x.content, pubication_date: x.publication_date }
+      end
+    end
   end
 
   def add_friend(user)
@@ -253,53 +245,47 @@ class User
   end
 
   def friends
-    array = @friends.map { |x| x = x.name }
-    result = @friends.map do |x|
-                if array.count(x.name) > 1
-                  x = "#{x.name}(#{x.id})" 
-                else
-                  x = x.name
-                end
-             end
+    display_name
   end
 
   def send_message(friend, message)
     return unless @friends.include?(friend)
 
-    @messages.push({self.name => message})
-    friend.receive_message(self, message)
-  end
-
-  def receive_message(friend, message)
-    return unless @friends.include?(friend)
-
-    array = @friends.map { |x| x = x.name }
-    result = @friends.map do |x|
-                if array.count(x.name) > 1
-                  x = "#{x.name}(#{x.id})" 
-                else
-                  x = x.name
-                end
-             end
+    result = display_name
     index = @friends.index(friend)
-    @messages.push({result[index] => message})
+    @messages.push({"Me to #{result[index]}" => message})
+    friend.receive_message(self, message)
   end
 
   def conversation(friend)
     return [] unless @friends.include?(friend)
 
-    array = @friends.map { |x| x = x.name }
-    array = @friends.map do |x|
-                if array.count(x.name) > 1
-                  x = "#{x.name}(#{x.id})" 
-                else
-                  x = x.name
-                end
-             end
+    array = display_name
     index = @friends.index(friend)
-    result = @messages.keep_if do |x|
-                x.key?(self.name) || x.key?(array[index])
-             end
+    @messages.keep_if do |x|
+      (["Me to #{array[index]}", array[index]] & x.keys).any?
+    end
+  end
+
+  def receive_message(friend, message)
+    return unless @friends.include?(friend)
+
+    result = display_name
+    index = @friends.index(friend)
+    @messages.push({result[index] => message})
+  end
+
+  private
+
+  def display_name
+    array = @friends.map { |x| x.name }
+    @friends.map do |x|
+      if array.count(x.name) > 1
+        "#{x.name}(#{x.id})" 
+      else
+        x.name
+      end
+    end
   end
 end
 
@@ -317,7 +303,6 @@ end
   # p first_user.organisations # [BbcNews]
   # p second_user.organisations # []
   # p third_user.organisations # [CnnNews, BbcNews]
-  
   # p first_user.show_news(BbcNews) 
   # [{title: "BBC second news!", content: "Hello Ruby from Asiantech", category: "Say hello"},
   #  {title: "BBC third news", content: "Hello AsianTech from BBC", category: "Say hello"}]
@@ -341,7 +326,7 @@ end
   first_user.send_message(second_user, "I just came to say hello.")
   second_user.send_message(first_user, "...")
   
-  # p second_user.conversation(first_user)
+  p second_user.conversation(first_user)
   # [{ "Bob" => "Hello!" },
   #  { "Smith" => "Hi! what"s up?" },
   #  { "Bob" => "I just came to say hello."}
@@ -350,6 +335,6 @@ end
   first_user.send_message(third_user, "Hello!")
   first_user.send_message(third_user, "I just came to say hello.")
     
-  p first_user.messages
+  # p first_user.messages
   p first_user.conversation(third_user) # []
   # p third_user.conversation(first_user) # []
