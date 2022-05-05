02c3c88cefd8d2fac8d84031672abc0a623d609e
Hau Phan T. VN.Danang <hau.phan@monstar-lab.com>
2022-05-05 09:27:18 +0700

    W2-D3, Hậu Phan : Hoàn thành bài tập 4

--- mvn-hauphan/w2-d3/block-proc-lambda/exercise_4.rb
+++ mvn-hauphan/w2-d3/block-proc-lambda/exercise_4.rb
@@ -27,10 +27,32 @@ class HtmlToText
   end
 
   def show_view
+    @html.gsub!('<br>', '<div>')
+    @html.gsub!('</br>', '<div>')
+    a = @html.split('<div>')
+    a.map! do |x| 
+      if x.include?('</div>') 
+        x.split('</div>')
+      else
+        x
+      end
+    end
+    a.each do |x|
+      if x.class == Array
+        if x.length == 2
+          p x[0].strip
+          p x[1].strip
+        else
+          p x[0].strip
+          p x[1..x.length-1].join.strip
+        end
+      else
+        p x.strip
+      end
+    end
   end
-
 end
 
 a = HtmlToText.new
 a.get_html('<div>Hello Ruby</div><<<br>>><div>Will</div> be</div>Fun<p></p>')
-p a.show_view
+a.show_view
