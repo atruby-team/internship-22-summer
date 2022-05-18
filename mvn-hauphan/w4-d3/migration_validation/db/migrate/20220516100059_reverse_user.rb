851d23b20121343a8165ffb63c138f731aa66114
Hau Phan T. VN.Danang <hau.phan@monstar-lab.com>
2022-05-18 10:13:12 +0700

    W4-D3, Hậu Phan : migration + validation

--- mvn-hauphan/w4-d3/migration_validation/db/migrate/20220516100059_reverse_user.rb
+++ mvn-hauphan/w4-d3/migration_validation/db/migrate/20220516100059_reverse_user.rb
@@ -1,4 +1,9 @@
 class ReverseUser < ActiveRecord::Migration[7.0]
   def up
+    term = []
+    User.all.reverse_each { |x| term.push(x) }
+    term.each_with_index { |x, index| x.id = index + 1 }
+    User.all.drop(User.all.size)
+    term.each { |x| User.create(email: x.email, password: x.password, gender: x.gender, single?: x.single?, favorite_song: x.favorite_song, team_id: x.team_id) }
   end
 end
