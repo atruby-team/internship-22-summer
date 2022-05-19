ee90107d60a5cee3cddb78328a9304623b05c1a9
Hau Phan T. VN.Danang <hau.phan@monstar-lab.com>
2022-05-19 10:21:41 +0700

    W5-D1, Hau Phan : Fix comment bt8

--- mvn-hauphan/w5-d1/model_callback_association_query_interface/app/models/comment.rb
+++ mvn-hauphan/w5-d1/model_callback_association_query_interface/app/models/comment.rb
@@ -1,13 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user, counter_cache: true
+  belongs_to :post, counter_cache: true
 
   validates :content, presence: true
-
-  after_create :update_comments_count
-  private
-    def update_comments_count
-      User.find(self.user_id).update(comments_count: User.find(self.user_id).comments_count += 1)
-      Post.find(self.post_id).update(comments_count: Post.find(self.post_id).comments_count += 1)
-    end
 end
--- mvn-hauphan/w5-d1/model_callback_association_query_interface/app/models/user.rb
+++ mvn-hauphan/w5-d1/model_callback_association_query_interface/app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   has_many :posts
-  has_many :comments
-  has_many :leaders, class_name: "Team", foreign_key: "leader_id"
+  has_many :comments, through: :posts
   has_many :teams_users
   has_many :teams, through: :teams_users
+  has_many :leaders, -> { distinct }, through: :teams, source: :leader
+  has_many :team_has_leaders, -> { where "leader_id not null" }, through: :teams_users, source: :team
+
+  scope :with_gender, -> (gender){ where("gender = #{gender}") }
+  scope :posts_count, -> { select("users.*", "count(posts.id) AS count").left_joins(:posts).group("users.id") }
 
   validates :email,presence: true,uniqueness: true, format: {with: /\A[a-zA-Z0-9]{3,10}@[a-zA-Z0-9]{3,10}.[a-zA-Z0-9]{2,5}\z/} 
   validates :phone_number,presence: true, format: {with: /\A[0-9]{9,10}\z/} 
